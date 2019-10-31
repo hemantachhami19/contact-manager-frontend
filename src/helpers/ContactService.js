2b9e9b3253906cd0297949907ef536ab33a8f327
Hemant Achhami <hemantachhami19@gmail.com>
2019-10-31 19:16:02 +0545

    minor change

--- src/helpers/ContactService.js
+++ src/helpers/ContactService.js
@@ -1,24 +1,25 @@
 // import React from 'react';
 import axios from 'axios'
-const baseUrl = "http://localhost:8000/api/v1/contacts"
 
- class ContactService {
+const baseUrl = "http://localhost:8000/api/v1/contacts";
 
-    static postApi(url,data){
-        return axios.post(baseUrl + url, data);   
-    }
+class ContactService {
+  
+  static postApi(url, data) {
+    return axios.post(baseUrl + url, data);
+  }
 
-    static putApi(url,data){
-         return axios.put(baseUrl + url, data);
-     }
+  static putApi(url, data) {
+    return axios.put(baseUrl + url, data);
+  }
 
-    static getApi(url){
-        return axios.get(baseUrl + url);   
-    }
+  static getApi(url) {
+    return axios.get(baseUrl + url);
+  }
 
-    static deleteApi(url){
-        return axios.delete(baseUrl + url);   
-    }
+  static deleteApi(url) {
+    return axios.delete(baseUrl + url);
+  }
 
 }
 
